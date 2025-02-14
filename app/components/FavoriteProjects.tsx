aea7a93bf6f3d836ee0e7668a0dea75121478f61
Nitrowritescode <swarsehgal123@gmail.com>
2025-02-14 14:51:23 +0530

    added toast

--- app/components/FavoriteProjects.tsx
+++ app/components/FavoriteProjects.tsx
@@ -1,29 +1,36 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 export default function Projects() {
   return (
-    <section className="projects-container">
+    <div className='px-10 md:px-16'>
+      <h1 className="text-purple-500 text-2xl md:text-4xl font-bold flex justify-center items-center py-4">My Projects</h1>
+      <section className="projects-container bg-purple-400 p-4">
     {/* Project Card 1 */}
-    <div className="project-card">
+    <div className="p-4">
       {/* <img src="/images/project1.jpg" alt="Screenshot of Project 1" /> */}
-      <h3>Stories On Tips</h3>
-      <p>
+      <h3 className='font-bold text-2xl md:text-3xl'>Stories On Tips</h3>
+      <p className='py-2 '>
       Full stack story generator production grade SaaS application using Next JS 15, React 19 and Typescript, including optimistic UI updates.
       </p>
       <a href="https://storiesontips.com" target="_blank" rel="noopener noreferrer">
+     <Button>
   View Project
+      </Button> 
 </a>
     </div>
     
     {/* Project Card 2 */}
-    <div className="project-card">
+    <div className="p-4">
       {/* <img src="/images/project2.jpg" alt="Screenshot of Project 2" /> */}
-      <h3>My Invoice Checker</h3>
-      <p>
-        Full Stack Invoice Management App for small businesses.
+      <h3 className='font-bold text-2xl md:text-3xl'>My Invoice Checker</h3>
+      <p className='py-2'>
+        Full Stack Invoice Management App for small businesses using Next JS 15, React 19 and Typescript
       </p>
       <a href="https://myinvoicechecker.com" target="_blank" rel="noopener noreferrer">
+      <Button>
   View Project
+      </Button> 
 </a>
     </div>
     
@@ -32,5 +39,6 @@ export default function Projects() {
     
     {/* Add more project cards as needed */}
   </section>
+    </div>
   )
 }
